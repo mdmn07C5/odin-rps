72c823fd979ef10136ea7d254cc1ee73952d0ae1
mdmn07C5 <malicdan@protonmail.ch>
2024-02-28 12:59:05 -0500

    Adds win condition
    Alert pops up if either player reaches a score of 5 signalling the end
    
    Also refactored a bunch of stuff, most logic are now enclosed in their
    own functions.

--- index.js
+++ index.js
@@ -6,71 +6,66 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection)
-        return "It's a tie, WAOW!";
+        return ["tie", playerSelection, computerSelection]
 
-    if (playerSelection === "rock" && computerSelection === "scissors") 
-        return "You win!";
-    if (playerSelection === "scissors" && computerSelection === "paper") 
-        return "You win!";
-    if (playerSelection === "paper" && computerSelection === "rock") 
-        return "You win!";
-
-    return `You lose, ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "rock") 
+        return ["player", playerSelection, computerSelection];
+  
+    return ["computer", playerSelection, computerSelection]
 }
 
-function playGame() {
-    let rounds = 5
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < rounds; i++) {
-        let playerChoice = prompt("Rock, Paper, or Scissors?: ");
-        let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerChoice);
-        console.log(result);
-        if (result === "You win!") {
-            playerScore += 1;
-        } else {
-            computerScore += 1;
-        }
+function checkResults(result) {
+    const winner = result[0]
+    
+    const id = `#${winner}-score`
+    let score = 0
+
+    if (winner === 'player') {
+        updateStatus(result);
+        score = updateScore(document.querySelector(id));
+    } else if (winner === 'computer') {
+        updateStatus(result);
+        score = updateScore(document.querySelector(id));
+    } else {
+        updateStatus(result);
     }
-    console.log(`You: ${playerScore}, Computer: ${computerScore}`)
-}
 
-const scoreChangedEvent = new CustomEvent('scoreChangedEvent');
+    if (score === 5) {
+        alert(`${winner} wins the match!`);
+    }
+}
 
 function updateScore(scoreContainer) {
     const currentScore = Number.parseInt(scoreContainer.textContent);
     scoreContainer.textContent = currentScore + 1;
+    return currentScore + 1
 }
 
-const container = Array.from(document.querySelectorAll('#container button'));
-const playerScoreContainer = document.querySelector('#player-score');
-const computerScoreContainer = document.querySelector('#computer-score');
-const resultContainer = document.querySelector('#result');
-const scoreContainer = document.querySelector('#score');
+function updateStatus(result) {
+    const statusContainer = document.querySelector('#status');
+    text = ``
+    if (result[0] === 'tie') 
+        text = `it's a tie!`
+    else 
+        text = `${result[0]} wins! player chose ${result[1]}, computer chose ${result[2]}`
+    statusContainer.textContent = text
+}
 
-scoreContainer.addEventListener('scoreChangedEvent', () =>{
-    console.log("the score was changed");
-})
+function registerEventsToButtons(){
+    const container = Array.from(document.querySelectorAll('#container button'));
 
-
-container.forEach((button) => 
+    container.forEach((button) => 
     button.addEventListener('click', () => {
         const playerChoice = button.textContent;
         const computerChoice = getComputerChoice();
+
         let result = playRound(playerChoice, computerChoice);
 
-        if (result === 'You win!') {
-            updateScore(playerScoreContainer);
-            scoreContainer.dispatchEvent(scoreChangedEvent);
-        } else if (result !== "It's a tie, WAOW!") {
-            updateScore(computerScoreContainer);
-            scoreContainer.dispatchEvent(scoreChangedEvent);
-        }
+        checkResults(result);
 
-        resultContainer.textContent = `
-            You chose ${playerChoice}, Computer chose ${computerChoice}.
-            ${result}
-        `;
-    })
-)
+    }));
+}
+
+registerEventsToButtons();
